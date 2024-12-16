64f8f78a098ab777c610b578c3db5dc67341a923
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:14:57 +0700

    membuat class HomeMhsViewModel

---
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum10.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+):ViewModel(){
+
+}
