bd7298ed79f3fdf50336cb3e0a626eaacd6fcea2
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:34:06 +0700

    membuat class DetailMhsViewModel dengan operasi yang terdapat di repositorymhs

---
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum10.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.repository.RepositoryMhs
+import com.example.praktikum10.ui.navigation.DestinasiDetail
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+):ViewModel(){
+
+}
