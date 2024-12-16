2734973e6e2c67630660a9b3782ccae1898a99ec
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 01:13:13 +0700

    membuat scaffold dan floatingbutton untuk add denga icons

--- app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,17 @@
 package com.example.praktikum10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum10.ui.customwidget.TopAppBar
 import com.example.praktikum10.ui.viewmodel.HomeMhsViewModel
 import com.example.praktikum10.ui.viewmodel.PenyediaViewModel
 
@@ -13,5 +22,26 @@ fun HomeMhsView(
     onDetailClick: (String) -> Unit = { },
     modifier: Modifier = Modifier
 ){
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes. medium,
+                modifier = Modifier.padding(16.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+            }
+        }
+    ) {}
 }
