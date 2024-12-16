06f398a5b93bd239b527459571f32e6770be251c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:20:02 +0700

    membuat data class HomeUIState

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,13 +1,26 @@
 package com.example.praktikum10.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.praktikum10.data.entity.Mahasiswa
 import com.example.praktikum10.repository.RepositoryMhs
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
+data class HomeUIState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+
+)
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ):ViewModel(){
     val homeUIState: StateFlow<HomeUIState> = repositoryMhs.getAllMhs()
         .filterNotNull()
+        .map {
+            HomeUIState(
+                listMhs
+            )
+        }
 }
