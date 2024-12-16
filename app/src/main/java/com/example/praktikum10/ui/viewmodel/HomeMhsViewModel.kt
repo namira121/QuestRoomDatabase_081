d990e228b48db1b2688b2e4ea5841bf0398ffb2e
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:17:44 +0700

    membuat variabel homeuistate

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,9 +2,12 @@ package com.example.praktikum10.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.praktikum10.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ):ViewModel(){
-
+    val homeUIState: StateFlow<HomeUIState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
 }
