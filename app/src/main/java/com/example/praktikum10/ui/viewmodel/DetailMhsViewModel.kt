b36888470a006553a43f0a72d8abf3076b3f4d22
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:38:52 +0700

    menambahkan filterNotNull di class DetailMhsViewModel

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/DetailMhsViewModel.kt
@@ -4,10 +4,15 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.praktikum10.repository.RepositoryMhs
 import com.example.praktikum10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ):ViewModel(){
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUIState: StateFlow<DetailUIState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
 }
