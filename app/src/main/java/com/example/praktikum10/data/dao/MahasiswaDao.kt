a98b37aa3f016e0f39b39331d8273e66fe61c6c5
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 22:14:42 +0700

    membuat metode untuk mengambil semua data mahasiswa dari database

--- app/src/main/java/com/example/praktikum10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum10/data/dao/MahasiswaDao.kt
@@ -2,7 +2,9 @@ package com.example.praktikum10.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.praktikum10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -11,4 +13,7 @@ interface MahasiswaDao {
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa //yang dipanggil tabelnya
     )
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
