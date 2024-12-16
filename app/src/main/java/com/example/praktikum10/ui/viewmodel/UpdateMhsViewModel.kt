19509ac24c3140aac570f1e5db3cc2728322194b
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 00:05:34 +0700

    membuat class UpdateMhsViewModel

---
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum10.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+): ViewModel(){
+
+}
