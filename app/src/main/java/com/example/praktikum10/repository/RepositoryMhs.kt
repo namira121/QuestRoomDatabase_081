802e2b4fd3991ff9174fda01dbdd00f6ea928717
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 22:24:16 +0700

    membuat metode untuk memanggil fungsi getAllMahasiswa

--- app/src/main/java/com/example/praktikum10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum10/repository/RepositoryMhs.kt
@@ -1,7 +1,10 @@
 package com.example.praktikum10.repository
 
 import com.example.praktikum10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
