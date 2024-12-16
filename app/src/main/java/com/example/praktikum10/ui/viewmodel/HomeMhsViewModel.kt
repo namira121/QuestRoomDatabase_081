1d71f74e846578331fd26d913c820ec88dacfc9a
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:26:44 +0700

    menambahkan catch di class HomeMhsViewModel

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeMhsViewModel.kt
@@ -5,6 +5,7 @@ import com.example.praktikum10.data.entity.Mahasiswa
 import com.example.praktikum10.repository.RepositoryMhs
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -29,6 +30,15 @@ class HomeMhsViewModel(
         .onStart {
             emit(HomeUIState(isLoading = true))
             delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?:"Terjadi kesalahan"
+                )
+            )
         }
 
 
