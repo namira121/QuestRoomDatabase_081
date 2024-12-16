ef2eee33435b341060b44531f25d6941272648ad
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 00:11:33 +0700

    membuat metode updatestate

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -5,8 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum10.repository.RepositoryMhs
 import com.example.praktikum10.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -16,4 +20,19 @@ class UpdateMhsViewModel(
         private set
 
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState=repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+    fun UpdateState(mahasiswaEvent: MahasiswaEvent){
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
 }
