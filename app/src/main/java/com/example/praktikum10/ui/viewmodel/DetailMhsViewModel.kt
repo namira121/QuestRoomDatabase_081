44f60fe80228f23e261973f6958d8f235dbd6c1e
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 23:45:04 +0700

    menambahkan stateIn di class DetailMhsViewModel

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,12 +2,15 @@ package com.example.praktikum10.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum10.repository.RepositoryMhs
 import com.example.praktikum10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -32,4 +35,11 @@ class DetailMhsViewModel(
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DatailUIState(
+                isLoading = true,
+            )
+        )
 }
