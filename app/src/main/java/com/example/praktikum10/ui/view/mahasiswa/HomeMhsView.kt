bf4f525d0c81ced0194582239a20d8da5c3f0915
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 06:02:20 +0700

    menampilkan indikator loading

--- app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,20 +1,29 @@
 package com.example.praktikum10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum10.ui.customwidget.TopAppBar
 import com.example.praktikum10.ui.viewmodel.HomeMhsViewModel
+import com.example.praktikum10.ui.viewmodel.HomeUIState
 import com.example.praktikum10.ui.viewmodel.PenyediaViewModel
 
 @Composable
@@ -55,5 +64,26 @@ fun HomeMhsView(
             },
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUIState: HomeUIState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState()}
+    when{
+        homeUIState.isLoading -> {
+            Box(
+                modifier=modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+              CircularProgressIndicator()
+            }
+        }
+
     }
 }
