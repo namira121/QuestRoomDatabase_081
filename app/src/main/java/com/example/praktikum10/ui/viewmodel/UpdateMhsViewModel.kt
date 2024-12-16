3d140c26e176f49b5fb853f2cae211c715997189
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 00:08:44 +0700

    mengecek apakah nim null atau tidak

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.praktikum10.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.praktikum10.repository.RepositoryMhs
+import com.example.praktikum10.ui.navigation.DestinasiUpdate
 
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ): ViewModel(){
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
 
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
 }
