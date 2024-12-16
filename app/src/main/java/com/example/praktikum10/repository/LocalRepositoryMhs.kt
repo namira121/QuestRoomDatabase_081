691ac43ca5f805dbddfd1eded644e17bdfdae361
Namira Pramadhani <namnam6617@gmail.com>
2024-12-16 22:35:26 +0700

    membuat metode untuk memanggil fungsi getAllMahasiswa dari MahasiswaDao

--- app/src/main/java/com/example/praktikum10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum10/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.praktikum10.repository
 
 import com.example.praktikum10.data.dao.MahasiswaDao
 import com.example.praktikum10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,6 @@ class LocalRepositoryMhs(
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
