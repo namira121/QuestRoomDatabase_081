ae06e25016145405eecf5f8b35b99b526ade01b7
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 08:56:33 +0700

    Mengatur ukuran dan warna judul dan isinya

--- app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
@@ -25,7 +25,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Delete
 import com.example.praktikum10.data.entity.Mahasiswa
@@ -174,5 +177,18 @@ fun ComponentDetailMhs(
     judul: String,
     isinya: String,
 ){
-
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+       Text(text = "$judul",
+           fontSize = 20.sp,
+           fontWeight = FontWeight.Bold,
+           color = Color.Gray
+       )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
