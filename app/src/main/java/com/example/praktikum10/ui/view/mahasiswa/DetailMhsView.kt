54584930970b085ae65a3bcac68fe7c1498fac6b
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 07:35:43 +0700

    menambahkan scaffold untuk judul halaman, tombol balik dan button untuk mengedit

--- app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,9 +1,19 @@
 package com.example.praktikum10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Delete
+import com.example.praktikum10.ui.customwidget.TopAppBar
 import com.example.praktikum10.ui.viewmodel.DetailMhsViewModel
 import com.example.praktikum10.ui.viewmodel.PenyediaViewModel
 
@@ -15,5 +25,37 @@ fun DetailMhsView(
     onEditClick: (String) -> Unit = { },
     onDeleteClick: () -> Unit = { }
 ) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUIState.value.detailUiEvent.nim) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                    )
+            }
+        }
+    ){innerPadding ->
+        val detailUIState by viewModel.detailUIState.collectAsState()
 
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUIState = detailUIState,
+            onDeleteClick = {
+                viewModel.deleteMhs()
+                onDeleteClick()
+            }
+        )
+    }
 }
