d93499597d570814e518815474344214325e5c01
Namira Pramadhani <namnam6617@gmail.com>
2024-12-17 08:34:26 +0700

    membuat konfirmasi sebelum menghapus data dan menambahkan modifier di TopAppBar

--- app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,15 +1,27 @@
 package com.example.praktikum10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -18,6 +30,7 @@ import com.example.praktikum10.ui.customwidget.TopAppBar
 import com.example.praktikum10.ui.viewmodel.DetailMhsViewModel
 import com.example.praktikum10.ui.viewmodel.DetailUIState
 import com.example.praktikum10.ui.viewmodel.PenyediaViewModel
+import com.example.praktikum10.ui.viewmodel.toMahasiswaEntity
 
 @Composable
 fun DetailMhsView(
@@ -68,5 +81,57 @@ fun BodyDetailMhs(
     detailUIState: DetailUIState = DetailUIState(),
     onDeleteClick: () -> Unit = { }
 ){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUIState.isLoading ->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
 
+        detailUIState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    . padding(16.dp)
+            ) {
+                ItemDetailMhs(
+                    mahasiswa = detailUIState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if(deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteClick = {deleteConfirmationRequired =false},
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        detailUIState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
 }
